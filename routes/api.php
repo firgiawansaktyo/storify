984a997d080ae21fecbf52eeb001be109ef652c8
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-22 23:04:43 +0700

    image modal s3

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\Api\ImageModalController;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(['throttle:api', 'apikey'])
-//     ->get('pictures/{id}', [ImageModalController::class, 'show'])
-//     ->name('api.pictures.show');
+
