efc941730a330e6919e6ab51a923d9ca8e549ced
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-23 15:17:19 +0700

    landing text cover

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware
-        ->append(SecurityHeaders::class)
+        // ->append(SecurityHeaders::class)
         ->alias([
             'apikey' => RequireApiKey::class,
         ]);
