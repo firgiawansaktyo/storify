2bfddef7d495b9a986150aae4bf8de35c064e2ed
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-23 23:48:46 +0700

    timeline

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -1,29 +1,58 @@
-<div class="container-sweetvows bg-[var(--spotify-gray-bold)]">
-    <h2 class="text-xl font-bold">Timeline</h2>
-    <div class="timeline pl-5 rounded-xl">      
-        <ol class="relative border-s border-white">                  
-            <li class="mb-10 ms-6 ml-8">            
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-[var(--spotify-gray-bold)] rounded-full -start-3 ring-8 ring-white">
-                    <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<div class="container-sweetvows bg-[var(--spotify-gray-bold)] p-5 rounded-xl">
+    <h2 class="text-xl font-bold mb-4">Timeline</h2>
+
+    <div class="timeline pl-5">
+        <ol class="relative border-s border-white">
+            <li class="mb-10 ms-6 ml-8">
+                <span class="absolute flex items-center justify-center w-6 h-6 
+                    bg-[var(--spotify-gray-bold)] rounded-full -start-3 ring-8 ring-white">
+                    <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                         fill="currentColor" viewBox="0 0 20 20">
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                     </svg>
                 </span>
-                <h3 class="flex items-center mb-1 text-xl font-semibold">Akad</h3>
-                <time class="block mb-1 text-sm font-normal leading-none text-[var(--spotify-gray)]">8 February 2025, 08:00-10:00 WIB</time>
-                <p class="mb-1 text-base font-normal text-[var(--spotify-gray)]">The Alana Hotel & Conference Center - Sentul City</p>
-                <img src="{{ Storage::disk(env('FILESYSTEM_DISK'))->url($wedding->wedding_vow_image) }}" alt="Lana Del Rey Born To Die" class="w-1/4 h-auto rounded-lg" />
+
+                <h3 class="mb-1 text-xl font-semibold">Akad</h3>
+                <time class="block mb-1 text-sm font-normal text-[var(--spotify-gray)]">
+                    {{ \Carbon\Carbon::parse($wedding->wedding_vow_date)->format('d F Y') }},
+                    {{ $wedding->wedding_vow_start_time }} - {{ $wedding->wedding_vow_end_time }} WIB
+                </time>
+                <p class="mb-1 text-base font-normal text-[var(--spotify-gray)]">
+                    {{ $wedding->wedding_vow_location }}
+                </p>
+                @if ($wedding->wedding_vow_image)
+                    <img src="{{ Storage::disk(env('FILESYSTEM_DISK'))->url($wedding->wedding_vow_image) }}"
+                         alt="Akad Image"
+                         class="w-1/4 h-auto rounded-lg mt-2" />
+                @endif
             </li>
-            <li class="ms-6 ml-8">            
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-[var(--spotify-gray-bold)] rounded-full -start-3 ring-8 ring-white">
-                    <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <li class="ms-6 ml-8">
+                <span class="absolute flex items-center justify-center w-6 h-6 
+                    bg-[var(--spotify-gray-bold)] rounded-full -start-3 ring-8 ring-white">
+                    <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                         fill="currentColor" viewBox="0 0 20 20">
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                     </svg>
                 </span>
-                <h3 class="flex items-center mb-1 text-xl font-semibold">Resepsi</h3>
-                <time class="block mb-1 text-sm font-normal leading-none text-[var(--spotify-gray)]">8 February 2025, 11:00-14:00 WIB</time>
-                <p class="text-base font-normal text-[var(--spotify-gray)]">The Alana Hotel & Conference Center - Sentul City</p>
-                <img src="{{ Storage::disk(env('FILESYSTEM_DISK'))->url($wedding->wedding_reception_image) }}" alt="Lana Del Rey Born To Die" class="w-1/4 h-auto rounded-lg" />
+
+                <h3 class="mb-1 text-xl font-semibold">Resepsi</h3>
+
+                <time class="block mb-1 text-sm font-normal text-[var(--spotify-gray)]">
+                    {{ \Carbon\Carbon::parse($wedding->wedding_reception_date)->format('d F Y') }},
+                    {{ $wedding->wedding_reception_start_time }} - {{ $wedding->wedding_reception_end_time }} WIB
+                </time>
+
+                <p class="text-base font-normal text-[var(--spotify-gray)]">
+                    {{ $wedding->wedding_reception_location }}
+                </p>
+
+                @if ($wedding->wedding_reception_image)
+                    <img src="{{ Storage::disk(env('FILESYSTEM_DISK'))->url($wedding->wedding_reception_image) }}"
+                         alt="Resepsi Image"
+                         class="w-1/4 h-auto rounded-lg mt-2" />
+                @endif
             </li>
+
         </ol>
     </div>
 </div>
