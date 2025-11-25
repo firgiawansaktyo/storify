748b710f6261de872edfd927ddbf2962a1ababa6
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-25 22:22:01 +0700

    tooltip

--- resources/js/playcontrol.js
+++ resources/js/playcontrol.js
@@ -1,27 +1,33 @@
+document.getElementById('closeTooltip').addEventListener('click', function() {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+});
+
 const video = document.getElementById('video-banner');
 const audio = document.getElementById('audio-banner');
-const btn    = document.getElementById('playPause');
-const btnFloat    = document.getElementById('playPauseFloat');
-const playIcon  = document.getElementById('playIcon');
+const btn = document.getElementById('playPause');
+const btnFloat = document.getElementById('playPauseFloat');
+const playIcon = document.getElementById('playIcon');
 const pauseIcon = document.getElementById('pauseIcon');
-const playIconFloat  = document.getElementById('playIconFloat');
+const playIconFloat = document.getElementById('playIconFloat');
 const pauseIconFloat = document.getElementById('pauseIconFloat');
-const currT   = document.getElementById('currentTime');
-const durT    = document.getElementById('duration');
+const currT = document.getElementById('currentTime');
+const durT = document.getElementById('duration');
 const progBar = document.getElementById('progressBar');
 let wasPlaying = false;
-let wasEnded   = false;
+let wasEnded = false;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 350) {
-    floatingPlayer.classList.remove('hidden');
-  } else {
-    floatingPlayer.classList.add('hidden');
-  }
+    if (window.scrollY > 350) {
+        floatingPlayer.classList.remove('hidden');
+    } else {
+        floatingPlayer.classList.add('hidden');
+    }
 });
 
 const fmt = t => {
-    const m = Math.floor(t/60), s = Math.floor(t%60).toString().padStart(2,'0');
+    const m = Math.floor(t / 60), s = Math.floor(t % 60).toString().padStart(2, '0');
     return `${m}:${s}`;
 };
 
@@ -30,34 +36,46 @@ video.addEventListener('loadedmetadata', () => {
 });
 
 btn.addEventListener('click', () => {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+    
     if (video.paused) {
-    video.play(); audio.play();
-    playIcon.classList.add('hidden');
-    playIconFloat.classList.add('hidden');
-    pauseIcon.classList.remove('hidden');
-    pauseIconFloat.classList.remove('hidden');
+        video.play();
+        audio.play();
+        playIcon.classList.add('hidden');
+        playIconFloat.classList.add('hidden');
+        pauseIcon.classList.remove('hidden');
+        pauseIconFloat.classList.remove('hidden');
     } else {
-    video.pause(); audio.pause();
-    pauseIcon.classList.add('hidden');
-    pauseIconFloat.classList.add('hidden');
-    playIcon.classList.remove('hidden');
-    playIconFloat.classList.remove('hidden');
+        video.pause();
+        audio.pause();
+        pauseIcon.classList.add('hidden');
+        pauseIconFloat.classList.add('hidden');
+        playIcon.classList.remove('hidden');
+        playIconFloat.classList.remove('hidden');
     }
 });
 
 btnFloat.addEventListener('click', () => {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+    
     if (video.paused) {
-    video.play(); audio.play();
-    playIcon.classList.add('hidden');
-    playIconFloat.classList.add('hidden');
-    pauseIcon.classList.remove('hidden');
-    pauseIconFloat.classList.remove('hidden');
+        video.play();
+        audio.play();
+        playIcon.classList.add('hidden');
+        playIconFloat.classList.add('hidden');
+        pauseIcon.classList.remove('hidden');
+        pauseIconFloat.classList.remove('hidden');
     } else {
-    video.pause(); audio.pause();
-    pauseIcon.classList.add('hidden');
-    pauseIconFloat.classList.add('hidden');
-    playIcon.classList.remove('hidden');
-    playIconFloat.classList.remove('hidden');
+        video.pause();
+        audio.pause();
+        pauseIcon.classList.add('hidden');
+        pauseIconFloat.classList.add('hidden');
+        playIcon.classList.remove('hidden');
+        playIconFloat.classList.remove('hidden');
     }
 });
 
@@ -65,14 +83,14 @@ video.addEventListener('timeupdate', () => {
     const pct = (video.currentTime / video.duration) * 100;
     progBar.style.width = pct + '%';
     currT.textContent = fmt(video.currentTime);
-        if (Math.abs(video.currentTime - audio.currentTime) > 0.3) {
+    if (Math.abs(video.currentTime - audio.currentTime) > 0.3) {
         audio.currentTime = video.currentTime;
-        }
+    }
 });
 
 video.addEventListener('ended', () => {
     audio.pause();
-    audio.currentTime  = 0;
+    audio.currentTime = 0;
     audio.playbackRate = 1;
     pauseIcon.classList.add('hidden');
     pauseIconFloat.classList.add('hidden');
@@ -81,21 +99,21 @@ video.addEventListener('ended', () => {
 });
 
 video.addEventListener('pause', () => {
-  audio.pause();
+    audio.pause();
 });
 
 document.addEventListener('visibilitychange', () => {
-  if (document.hidden) {
-    wasPlaying = !video.paused   && !video.ended;
-    wasEnded   = video.ended;
-    if (wasPlaying) {
-      video.pause();
-      audio.pause();
+    if (document.hidden) {
+        wasPlaying = !video.paused && !video.ended;
+        wasEnded = video.ended;
+        if (wasPlaying) {
+            video.pause();
+            audio.pause();
+        }
+    } else {
+        if (wasPlaying) {
+            video.play();
+            audio.play().catch(() => {});
+        }
     }
-  } else {
-    if (wasPlaying) {
-      video.play();
-      audio.play().catch(()=>{});
-    }
-  }
 });
