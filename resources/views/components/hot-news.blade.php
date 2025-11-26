8de5aa156f410199ef0306bffda45d9803ea1035
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-26 21:32:55 +0700

    fixing bridegroom and hotnews and floating button play

--- resources/views/components/hot-news.blade.php
+++ resources/views/components/hot-news.blade.php
@@ -1,6 +1,6 @@
 <div class="container-sweetvows bg-[var(--spotify-gray-bold)] max-w-3xl mx-auto">
     <h3 class="text-md font-bold">Hot News</h3>
-    <img class="rounded-lg max-w-full" src="{{ cdn_sweetvows($wedding->wedding_hotnews_image) }}">
+    <img class="rounded-lg max-w-full mb-2" src="{{ cdn_sweetvows($wedding->wedding_hotnews_image) }}">
     <p class="text-sm text-[var(--spotify-gray)]">
         {{ $wedding->wedding_hotnews_description }}
     </p>
