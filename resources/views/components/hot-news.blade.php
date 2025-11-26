23b20966309b8bd0e9f2c8cf2123f147207fe0e0
Firgiawan Saktyo <firgiawansaktyo@gmail.com>
2025-11-26 22:59:12 +0700

    add space

--- resources/views/components/hot-news.blade.php
+++ resources/views/components/hot-news.blade.php
@@ -1,7 +1,7 @@
 <div class="container-sweetvows bg-[var(--spotify-gray-bold)] max-w-3xl mx-auto">
-    <h3 class="text-md font-bold title-sweetvows mb-2">Hot News</h3>
-    <img class="rounded-lg max-w-full mb-2" src="{{ cdn_sweetvows($wedding->wedding_hotnews_image) }}">
-    <p class="text-sm text-[var(--spotify-gray)] mb-2">
+    <h3 class="text-md font-bold title-sweetvows mb-4">Hot News</h3>
+    <img class="rounded-lg max-w-full mb-3" src="{{ cdn_sweetvows($wedding->wedding_hotnews_image) }}">
+    <p class="text-sm text-[var(--spotify-gray)]">
         {{ $wedding->wedding_hotnews_description }}
     </p>
 </div>
